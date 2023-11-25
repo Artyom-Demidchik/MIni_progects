30e2bcdfe028058ae030fdf3ff8a82f6e53e7dbb
artem <artademod@gmail.com>
2023-11-25 03:08:47 +0300

    fix conditions for white cross

--- Rubiks solver/Solver.py
+++ Rubiks solver/Solver.py
@@ -1,10 +1,25 @@
 import Cube
-
+# staps = white_cross, full_white_cross, first_layer, second_layer, yellow_cross, corners_3_l, yellow_side, third_layer
 # byyyworrwyybwgbworoobroggborbbrrwggooryobyggwgwybywrgw
 # wwwwwwwwwgggggggggooooooooorrrrrrrrrbbbbbbbbbyyyyyyyyy
 cube = Cube.RubiksCube()
-white_cross = [cube.cube[0][0][1] == 'w', cube.cube[0][1][0] == 'w', cube.cube[0][1][2] == 'w',
-               cube.cube[0][2][1] == 'w']
+cube.distribute_colors(list(input('Enter your scramble -> ')))
+cube.print_cube()
+
+white_cross = bool
+
+total = 0
+while white_cross:
+    white_cross = (cube.cube[0][0][1] == 'w' and cube.cube[0][1][0] == 'w' and cube.cube[0][1][2] == 'w' and
+                   cube.cube[0][2][1] == 'w')
+    if white_cross:
+        cube.L_clockwise_rotation(1)
+        total += 1
+print(total)
+
+
+
+
 full_white_cross = ['wg', 'wo', 'wb', 'wr']
 first_layer = ['wro', 'wob', 'wbr', 'wrg']
 second_layer = ['go', 'ob', 'br', 'rg']
@@ -12,19 +27,3 @@ yellow_cross = ['y', 'y', 'y', 'y']
 corners_3_l = ['go', 'ob', 'br', 'rg']
 yellow_side = ['y', 'y', 'y', 'y']
 third_layer = ['yg', 'yo', 'yb', 'yr']
-
-staps = [white_cross, full_white_cross, first_layer, second_layer, yellow_cross, corners_3_l, yellow_side,
-         third_layer]
-cube.distribute_colors(list(input('Enter your scramble -> ')))
-
-
-def search_color():
-    pass
-
-
-cube.print_cube()
-total = 0
-while white_cross[:]:
-    cube.L_clockwise_rotation(1)
-    total += 1
-print(total)
